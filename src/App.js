7c2f02ac646705700ad083085cca12ca2658693a
Sahil Rajput <sahil@blulabs.in>
2021-07-09 19:50:18 +0530

    Launch sass and react with single script. ~Sahil

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./css/styles.css"; //Importing our compiled css from input sass.
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">Sass testing with newer @use imports.</div>;
 }
 
 export default App;
