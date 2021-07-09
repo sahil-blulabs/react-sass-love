02ff0bb1bf0ad28237a30f13a0c63deff08cd1a3
Sahil Rajput <sahil@blulabs.in>
2021-07-09 20:12:07 +0530

    Simple things

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import "./App.css";
-import "./css/styles.css"; //Importing our compiled css from input sass.
+import "./scss/main.scss";
 
 function App() {
   return <div className="App">Sass testing with newer @use imports.</div>;
