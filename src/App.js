be138d4c64eb13551bf7647186357a9b2e7a46a9
Sahil Rajput <sahil@blulabs.in>
2021-07-17 14:42:50 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 let l = console.log;
 
+const history = (path) => window.history.pushState("", "", path);
+
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
+
+      <div>
+        <button
+          onClick={() => {
+            history("/ama1");
+          }}
+        >
+          Click me to go to /ama1
+        </button>
+        <br />
+        <button
+          onClick={() => {
+            history("/");
+          }}
+        >
+          Click me to go to /
+        </button>
+      </div>
     </div>
   );
 }
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,18 @@
+const axios = require("axios");
+// NOTE: All below setup only works in development only, src: https://create-react-app.dev/docs/proxying-api-requests-in-development/
+
 function proxy(app) {
-  // app.get(/^\/$/, (req, res) => res.redirect("/list"));//~by kent c dodds to map `/` route to `/list`.
-  app.get("/ama1", (req, res) => res.send("Cool1"));
+  app.get(/^\/$/, (req, res) => res.redirect("/list")); //~by kent c dodds to map `/` route to `/list`.
+  app.get("/ama1", async (req, res) => {
+    const { data } = await axios({
+      method: "get",
+      url: "https://loveapi.ml/numbersapi/1/trivia",
+    });
+
+    // console.log("Data from api::", data);
+
+    res.send(data);
+  });
   app.get("/ama2", (req, res) => res.send("Cool2"));
   app.get("/ama3", (req, res) => res.send("Cool3"));
 }
