4aafec4f7e70f3192a1e2c01051de81cf1e0ea25
Sahil Rajput <sahil@blulabs.in>
2021-07-24 15:46:07 +0530

    Test redirect from and to and custom link comopnent, yo!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Link,
+  BrowserRouter,
+  Switch,
+  Redirect,
+  Route,
+  useHistory,
+} from "react-router-dom";
+import { forwardRef } from "react";
 
-function App() {
+const s = "cars";
+
+const Place = () => <div>I am place component.</div>;
+const Eifel = () => <div>I am eifel tower.</div>;
+
+const FancyLinkS = (props) => {
+  const history = useHistory();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div onClick={() => history.push(props.href)}>Go to /{props.children}</div>
+  );
+};
+
+const FancyLink = (props, ref) => {
+  const history = useHistory();
+  // FYI: Giving ref={ref}  in below span seems unnecessary to me coz it work withou it too.
+  return (
+    <div onClick={() => history.push(props.href)} ref={ref}>
+      Go to /{props.children}
     </div>
   );
-}
+};
 
-export default App;
+const FancyLinkF = forwardRef(FancyLink); // This is how its guided in react-router-dom documentation.
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      I am cars. <br />
+      <Link to="/" component={FancyLinkS} children={<span>Home</span>} />
+      <Link
+        to="/"
+        component={FancyLinkF}
+        children={
+          <span>
+            Home with forwarded ref. IDK what is it good for though :D
+          </span>
+        }
+      />
+      <a href="/">
+        Go to /Home with anchor tag ::FYI::CAUSES UNNECESSARY PAGE RELOAD
+        THOUGH. <br />
+      </a>
+      <Link to="/p" children="Go to /p" /> <br />
+      <Link to="/paris/tower" children="Go to /paris/tower" /> <br />
+      <Switch>
+        <Redirect from="/p" to="/place" />
+        <Redirect from="/paris/tower" to="/eifel" />
+        <Route path="/place" component={Place} />
+        <Route path="/eifel" component={Eifel} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
